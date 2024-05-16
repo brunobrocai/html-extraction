f760855dad88eda305022ca1e2ec5db7c640d412
Bruno Brocai <bruno-brocai@gmx.de>
2024-05-16 16:43:58 +0200

    Add functions to find irrelevant url patterns and html

---
+++ functions/explore_irrelevant.py
@@ -0,0 +1,83 @@
+import json
+import os
+import re
+
+
+def get_subdomains(urls, print_=False):
+    """Given an array of urls, return a set of subdomains.
+
+    Can also print the subdomains.
+    """
+
+    subdomains = set()
+    for url in urls:
+        subdomain_list = url.split('/')
+        subdomains.update(subdomain_list[:-1])
+
+    if print_:
+        for subdomain in subdomains:
+            print(subdomain)
+
+    return subdomains
+
+
+def list_key(json_dir, key):
+    """Lists all values of a key in a directory of json files."""
+    urls = set()
+    for root, _, files in os.walk(json_dir):
+        for file in files:
+            if file.endswith('.json'):
+                filepath = os.path.join(root, file)
+                with open(filepath, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    urls.add(data[key])
+    return urls
+
+
+def matching_set(set_, pattern):
+    """Gets the set of items in an array containing a regex pattern."""
+    matches = set()
+    for item in set_:
+        if re.search(pattern, item):
+            matches.add(item)
+    return matches
+
+
+def load_patterns(filepath):
+    """Load a list of patterns from a file."""
+    with open(filepath, 'r', encoding='utf-8') as f:
+        patterns = f.readlines()
+    patterns = [pattern.strip('\n') for pattern in patterns]
+    full_pattern = '|'.join(patterns)
+    return full_pattern
+
+
+def is_relevant(url, irrel_pattern):
+    """Check if a url is relevant based on a pattern."""
+    return not re.search(irrel_pattern, url)
+
+
+def apply_function_dir(directory, function, *args):
+    """Apply a function to all files in a directory."""
+    matching_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json'):
+                filepath = os.path.join(root, file)
+                with open(filepath, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                match = function(data, *args)
+                if match:
+                    matching_files.append(file)
+    return matching_files
+
+
+def print_match_results(
+    matches, filecount, list_matches=False
+):
+    if list_matches:
+        for match in matches:
+            print(match)
+
+    print(f"Number of matching files: {len(matches)}")
+    print(f'This represents {len(matches)/filecount:.2%} of the files.')
