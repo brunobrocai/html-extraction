7c0a98e6fff9000cf7ce0d8720b858c477d11ae9
Bruno Brocai <bruno-brocai@gmx.de>
2024-05-23 14:58:08 +0200

    Config now includes section about relevance checking functions

--- functions/config_parsing.py
+++ functions/config_parsing.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-def read_config(section, config_file='config.yaml'):
+def read_section(section, config_file='config.yaml'):
     with open(config_file, 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
 
@@ -9,7 +9,8 @@ def read_config(section, config_file='config.yaml'):
     try:
         irrelevant_domains = config[section]['irrelevant_subdomains']
         metadata_keep = config[section]['metadata_keep']
+        checker_functions = config[section]['checker_functions']
 
-        return irrelevant_domains, metadata_keep
+        return irrelevant_domains, metadata_keep, checker_functions
     except KeyError as e:
         raise KeyError("Section not found in config file.") from e
