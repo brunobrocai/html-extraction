c5f01eabe8cd76d3feb3880c9db167773c36dbe8
Bruno Brocai <bruno-brocai@gmx.de>
2024-06-23 12:21:25 +0200

    Improve markdown handling function, add new func to rename files

--- functions/utils.py
+++ functions/utils.py
@@ -1,4 +1,5 @@
 import re
+import os
 import markdown
 from bs4 import BeautifulSoup as BS
 
@@ -9,6 +10,7 @@ def markdown_to_text(text):
     https://gist.github.com/lorey/eb15a7f3338f959a78cc3661fbc255fe"""
 
     text = re.sub(r'\*\*(.*)\*\*', r' \1 ', text)
+    text = re.sub(r'\*\b(.*)\b\*', r' \1 ', text)
     text = re.sub(r'  +', ' ', text)
     lines = text.split('\n')
 
@@ -34,5 +36,28 @@ def markdown_to_text(text):
     return text
 
 
-def cleaner_markdown():
-    print()
+def rename_files_with_padded_index(directory):
+    # Get list of files in the directory
+    files = [
+        file for file in os.listdir(directory)
+        if not file.endswith('.py')
+        and os.path.isfile(os.path.join(directory, file))
+    ]
+
+    # Calculate the total number of digits needed for the highest index
+    num_files = len(files)
+    num_digits = len(str(num_files))
+
+    # Iterate over each file and rename it with a zero-padded index
+    for index, filename in enumerate(files):
+        # Construct zero-padded index
+        padded_index = str(index + 1).zfill(num_digits)
+
+        # Construct new filename with padded index
+        new_filename = f"{padded_index}.json"
+
+        # Rename the file
+        os.rename(
+            os.path.join(directory, filename),
+            os.path.join(directory, new_filename)
+        )
